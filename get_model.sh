f5b50e111f2869d1524d05b6a0fece89fc807e9e
juan-armond <juan.armond@6point6.co.uk>
2021-07-25 06:58:33 +0100

    added shell files

---
+++ get_model.sh
@@ -0,0 +1,4 @@
+wget https://public.ukp.informatik.tu-darmstadt.de/reimers/sentence-transformers/v0.2/paraphrase-MiniLM-L6-v2.zip
+unzip paraphrase-MiniLM-L6-v2.zip
+rm paraphrase-MiniLM-L6-v2.zip
+
